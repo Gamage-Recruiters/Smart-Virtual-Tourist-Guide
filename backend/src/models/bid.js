e5b8b83ec30cbcf07d0404069dc75f30535a31c7
Keshan Jayaweera <keshandinith4@gmail.com>
2026-05-31 13:22:00 +0530

    update code

--- backend/src/models/bid.js
+++ backend/src/models/bid.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
 const bidSchema = new mongoose.Schema({
-    tripId: { type: String, required: true },
-    driverName: { type: String, required: true },
-    bidAmount: { type: Number, required: true },
-    createdAt: { type: Date, default: Date.now }
+    tripId: 
+    { 
+        type: String, 
+        required: true 
+    },
+    driverName: 
+    { 
+        type: String, 
+        required: true 
+    },
+    bidAmount: 
+    { 
+        type: Number, 
+        required: true 
+    },
+    createdAt: 
+    {
+         type: Date, 
+         default: Date.now 
+    }
 });
 
 module.exports = mongoose.model('Bid', bidSchema);
