efcfc589efc82d779fdd90a0a1f10f20382e0798
KUMARA R.D.N.P. <rdnishanpkumara1122@gmail.com>
2026-06-02 14:21:50 +0530

    feat: create basic backend with db connection

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import dbConfig from '../config/db.config.js';
+
+const startServer = async () => {
+  await dbConfig();
+    const app = (await import('./app.js')).default;
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+}
