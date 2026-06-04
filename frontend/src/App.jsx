3951022cf2a1276ec575b578ef257bcf9ea9ac41
Shenith Madurawala <it22300096@my.sliit.lk>
2026-06-04 16:17:26 +0530

    Revert "Merge branch 'Booking-&-Reservation-System' into dev"
    This reverts commit 7a15560972ceefdcffb4fc2c1ce580dc32189fde, reversing
    changes made to 43963b39ad5fb5d2378b1d7392e9835fa80e60d1.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import MainPage from './pages/touristMainPage.jsx/mainPage.jsx';
-import Dashboard from './pages/touristDashboard/dashboard.jsx';
-import { RentVehiclePage } from './pages/rentVehicle/rentVehiclePage.jsx';
-import VehicleDetailsPage from './pages/rentVehicle/vehicleDetailsPage.jsx';
-import FindHotelPage from './pages/findHotel/FindHotelPage.jsx'
-import HotelDetails from './pages/findHotel/HotelDetails.jsx'
-import BookDriver from './pages/bookDriver/BookDriver.jsx'
-
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <Router>
-      <Routes>
-        {/* MainPage acts as the parent layout wrapper */}
-        <Route element={<MainPage />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/rent-vehicle" element={<RentVehiclePage />} />
-          <Route path="/vehicle-details/:id" element={<VehicleDetailsPage />} />
-          <Route path="/find-hotel" element={<FindHotelPage/>} />
-          <Route path="/hotel-details/:id" element={<HotelDetails/>} />
-          <Route path="/book-driver" element={<BookDriver/>} />
-        </Route>
-      </Routes>
-    </Router>    
+    <>
+      <h1 class="text-3xl font-bold underline"> Hello world!</h1>
+    </>
   )
 }
 
-export default App;
+export default App
--- frontend/src/components/touristRentVehicle/paginationButton.jsx
+++
@@ -1,8 +0,0 @@
-
-export const PaginationButton = ({ label, icon, active = false }) => (
-  <div className={`w-10 h-10 rounded-xl flex items-center justify-center font-bold text-sm transition-all ${
-    active ? 'bg-blue-600 text-white shadow-lg shadow-blue-100' : 'bg-white border border-slate-100 text-slate-600 hover:bg-slate-50'
-  }`}>
-    {label || icon}
-  </div>
-);
