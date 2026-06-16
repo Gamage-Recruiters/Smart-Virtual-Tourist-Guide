f4ca32b340c077ea435980cbafbace559cc9dc17
Lahiru075 <sanjeewalahiru057@gmail.com>
2026-06-16 14:14:18 +0530

    chore: resolve merge conflicts and integrate itinerary updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 
 import FinalTripReport from './pages/FinalTripReport';
@@ -24,30 +23,3 @@ function App() {
 }
 
 export default App
-=======
-import { useState } from 'react'
-
-import FinalTripReport from './pages/FinalTripReport';
-import FinalTripReportPDF from './pages/FinalTripReportPDF';
-import TouristArrivalReport from './pages/TouristArrivalReport';
-import RevenueReport from './pages/RevenueReport';
-import BehaviorStatReport from './pages/BehaviorStatReport';
-import ComplaintReport from './pages/ComplaintReport';
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      {/* <FinalTripReport /> */}
-      {/* <FinalTripReportPDF /> */}
-      {/* <TouristArrivalReport /> */}
-      {/* <RevenueReport /> */}
-      {/* <BehaviorStatReport /> */}
-      {/* <ComplaintReport /> */}
-    </>
-  )
-}
-
-export default App
->>>>>>> ebada5d5c29fc1294a4f406fc35c04308fdb4ea9
