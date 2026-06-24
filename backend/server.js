c13fee8908afe319394adbdc78a8c908cffe97b0
Aysha2711 <fathima.aysha2711@gmail.com>
2026-06-24 22:22:14 +0530

    Initial full project setup (backend + frontend, cleaned structure)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
+// const app = require('./src/app');
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = require('./src/app');
+const connectDB = require('./src/configs/db');
 
 const PORT = process.env.PORT || 5000;
+
+// connect database
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
