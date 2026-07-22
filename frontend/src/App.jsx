19200edd6581ccdf61af1ee2347de6db32e6e37d
Shenith Madurawala <it22300096@my.sliit.lk>
2026-07-22 15:41:47 +0530

    fix App.js issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+//Booking and reservation----------------------------------
+
 import BookingPage from './pages/booking&reservation/BookingPage.jsx'
 import ActivityBooking from './pages/more_details_services_booking/ActivityBooking.jsx'
 import HotelBooking from './pages/more_details_services_booking/HotelBooking.jsx'
@@ -8,9 +11,9 @@ import GuideBooking from './pages/more_details_services_booking/GuideBooking.jsx
 import RestaurantBooking from './pages/more_details_services_booking/RestaurantBooking.jsx'
 import MyBookings from './pages/booking&reservation/MyBookings.jsx'
 
-function App() {
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//Marketplace engine-------------------------------------
+
 import Home_Page from "./pages/Home_Page";
 import Drivers_Card from "./components/marketplace/Drivers_Card";
 import Vehicles_Card from "./components/marketplace/Vehicles_Card";
@@ -27,39 +30,34 @@ export default function App() {
   return (
     <Router>
       <Routes>
-      
-          <Route path="/booking-page" element={<BookingPage/>} />
-          <Route path="/activity-booking" element={<ActivityBooking/>} />
-          <Route path="/hotel-booking" element={<HotelBooking/>} />
-          <Route path="/driver-booking" element={<DriverBooking/>} />
-          <Route path="/vehicle-booking" element={<VehicleBooking/>} />
-          <Route path="/guide-booking" element={<GuideBooking/>} />
-          <Route path="/restaurant-booking" element={<RestaurantBooking/>} />
-          <Route path="/my-bookings" element={<MyBookings/>} />
+
+        <Route path="/booking-page" element={<BookingPage />} />
+        <Route path="/activity-booking" element={<ActivityBooking />} />
+        <Route path="/hotel-booking" element={<HotelBooking />} />
+        <Route path="/driver-booking" element={<DriverBooking />} />
+        <Route path="/vehicle-booking" element={<VehicleBooking />} />
+        <Route path="/guide-booking" element={<GuideBooking />} />
+        <Route path="/restaurant-booking" element={<RestaurantBooking />} />
+        <Route path="/my-bookings" element={<MyBookings />} /> 
+
+
+        <Route path="/" element={<Home_Page />}>
+          <Route index element={<Navigate to="/drivers" replace />} />
+          {/* Booking Marketplace URL */}
+          <Route path="drivers" element={<Drivers_Card />} />
+          <Route path="vehicles" element={<Vehicles_Card />} />
+          <Route path="guides" element={<Guides_Card />} />
+          <Route path="hotels" element={<Hotels_Card />} />
+          <Route path="restaurants" element={<Restaurants_Card />} />
+          <Route path="activities" element={<Activities_Card />} />
+        </Route>
+        <Route path="/driver-details" element={<Driver_Details />} />
+        <Route path="/other-drivers" element={<Submit_Bids />} />
+        <Route path="/ride-details" element={<Ride_Details />} />
+        <Route path="/submit-bids" element={<Driver_Bids />} />
 
       </Routes>
-    </Router>    
+    </Router>
   )
 }
 
-export default App;
-    <BrowserRouter>
-      <Routes>  
-        <Route path="/" element={<Home_Page />}>
-          <Route index element={<Navigate to="/drivers" replace />} />
-            {/* Booking Marketplace URL */}
-            <Route path="drivers" element={<Drivers_Card />} />
-            <Route path="vehicles" element={<Vehicles_Card />} />
-            <Route path="guides" element={<Guides_Card />} />
-            <Route path="hotels" element={<Hotels_Card />} />
-            <Route path="restaurants" element={<Restaurants_Card />} />
-            <Route path="activities" element={<Activities_Card />} />
-          </Route>
-            <Route path="/driver-details" element={<Driver_Details />} />
-            <Route path="/other-drivers" element={<Submit_Bids />} />
-            <Route path="/ride-details" element={<Ride_Details />} />
-            <Route path="/submit-bids" element={<Driver_Bids />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
