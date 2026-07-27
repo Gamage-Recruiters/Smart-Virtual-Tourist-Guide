a155035701fff47c4889880bddb709af55164f3d
Keshan Jayaweera <keshandinith4@gmail.com>
2026-07-27 22:21:56 +0530

    Revert "change the code for create work flow"
    This reverts commit 01fb77857ac2b84caeb50e5421c3a45f53355ad6.

--- frontend/src/pages/more_details_services_booking/DriverBooking.jsx
+++ frontend/src/pages/more_details_services_booking/DriverBooking.jsx
@@ -4,23 +4,22 @@ import DriverAvailabilityCard from "../../components/booking&reservation/service
 
 const DriverBooking = () => {
     return (
-        <div className="bg-gray-100 min-h-screen p-6 flex justify-center items-start pt-10">
-            <div className="w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-8">
-                <ServiceDetailsCard //hard coded------------------
-                    service={{
-                        image:
-                            "https://images.unsplash.com/photo-1500648767791-00dcc994a43e",
-                        name: "Kasun Perera",
-                        location: "Colombo, Sri Lanka",
-                        rating: 4.9,
-                        reviews: 187,
-                        description:
-                            "Professional tourist driver with 8+ years of experience and fluent English."
-                    }} 
-                />
+        <div className="bg-gray-100 min-h-screen p-6">
 
-                <DriverAvailabilityCard />
-            </div>
+            <ServiceDetailsCard //hard coded------------------
+                service={{
+                    image:
+                        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e",
+                    name: "Kasun Perera",
+                    location: "Colombo, Sri Lanka",
+                    rating: 4.9,
+                    reviews: 187,
+                    description:
+                        "Professional tourist driver with 8+ years of experience and fluent English."
+                }} />
+
+            <DriverAvailabilityCard />
+
         </div>
     )
 
