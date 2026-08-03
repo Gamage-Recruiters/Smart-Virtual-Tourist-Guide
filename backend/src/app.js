034ae526a8f1bfa257f3383bbeffa1dcd22ddbff
Shenith Madurawala <it22300096@my.sliit.lk>
2026-08-03 16:44:56 +0530

    fix bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,8 +21,8 @@ const app = express();
 
 // Middleware
 app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173' }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Connect to MongoDB
 connectDB();
